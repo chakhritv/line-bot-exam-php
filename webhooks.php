2e43c2af4760d7df5ae72b27ad07baab430cd95b
chakhritv <39007873+chakhritv@users.noreply.github.com>
2018-08-24 15:26:15 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -14,68 +14,44 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		/*
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['source']['userId'];
-			$text2 = $event['message']['text'];
-			$txt = 'userId:'.$text.'   message:'.$text2;
-			if (strncmp(strtolower($text2), 'reg:', strlen('reg:')) === 0){
-				$txt = 'Register success - userId:'.$text.'   outlet:'.substr($text2, strlen('reg:'));
-			}elseif (strtolower($text2) === 'vrp'){
-				$txt = 'your VRP score is 1999.99 points';
-			}elseif (strtolower($text2) === 'quick reply'){
-				$txt = 'quick reply';
-			}else{
-				$txt = 'echo.. : '.$text2;
-			}
+			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			
-				// Build message to reply back
-				$messages = [
-					'type' => 'text',
-					'text' => $txt
-				];
-
-
-		}else{
-			$event_type =  $event['type'];
-			$source_userid = $event['source']['userId'];
-			
-			$txt = 'event-type:'.$event_type.'    event-source-userId'.$source_userid;
-				
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+			$txt = 'source-type:' + $event['source']['type'];
+			$txt = $txt+' userId:' + $event['source']['userId'];
+			$txt = $txt+' groupId:' + $event['source']['groupId'];
+			$txt = $txt+' roomId :' + $event['source']['roomId '];
 			
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				'text' => $txt
 			];
-		}
-		*/
-		$replyToken = $event['replyToken'];
-		$txt = $events;
-		
-		// Make a POST Request to Messaging API to reply to sender
-		$url = 'https://api.line.me/v2/bot/message/reply';
-		$data = [
-			'replyToken' => $replyToken,
-			'messages' => [$messages],
-		];
-		$post = json_encode($data);
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		echo $result . "\r\n";
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+
+		}
+		
 		
 	}
 }
