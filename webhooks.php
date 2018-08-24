1458c00cad945a0c6b97be5376c2dc443e60985f
chakhritv <39007873+chakhritv@users.noreply.github.com>
2018-08-24 15:43:50 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -32,9 +32,10 @@ if (!is_null($events['events'])) {
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			//$url = 'https://api.line.me/v2/bot/message/reply';
+			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
-				'replyToken' => $replyToken,
+				'to' => $event['source']['userId'],
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
